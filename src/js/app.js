ded2252406829a432301dab51630153acee99df6
Harsh Patel <harshppatel2880@gmail.com>
2020-03-09 20:39:38 -0500

    Project Compelted

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,28 @@
-console.log('Hello World!');
+const targetElement = document.getElementById('content');
+
+const getClipData = () => {
+  navigator.permissions.query({ name: 'clipboard-read' })
+    .then(() => {
+      navigator.clipboard.readText()
+        .then((text) => {
+          if (!text) {
+            targetElement.innerHTML = 'We could not find anything in your clipboard!';
+            return;
+          }
+          targetElement.innerHTML = text.toString().trim();
+        })
+        .catch((err) => {
+          console.log(err);
+          targetElement.innerHTML = 'Oops! Something went wrong 😔.';
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      targetElement.innerHTML = 'Oops! Something went wrong 😔.';
+    });
+};
+
+window.onload = () => {
+  window.focus();
+  getClipData();
+};
